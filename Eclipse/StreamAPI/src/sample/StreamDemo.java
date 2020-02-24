71d5a7d6d9d2dc3a9b732ef86401ebf3d7b4d5b9
RaghulKumar <raghulcse5698@gmail.com>
2020-02-24 17:24:35 +0530

    commit_24022020_1

---
+++ Eclipse/StreamAPI/src/sample/StreamDemo.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamDemo {
+
+	public static void main(String args[]) {
+		System.out.println("Hello world");
+		List<String> listOfStrings = Arrays.asList("Java", "Oracle",
+				"JavaScript");
+		//stream functionality
+		listOfStrings.stream().filter(str -> str.length() > 5).findAny()
+		.ifPresent(System.out::println);
+	}
+}
